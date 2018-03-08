132e667e12b81c6c5e2b5aa29e674ddc53a7326c
d.trochain <d.trochain@it-akademy.fr>
2018-03-08 12:27:55 +0100

    Ajout route 408 et 404

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const router = require('./server/router')
 
 const app = express()
 
+/**
+ * Si la requête est effectué entre la 49éme et la 59éme minute, nous renvoyons une erreur 408. (timeout error)
+ */
+app.use((req, res, next) => {
+    const minutes = new Date().getMinutes()
+    if (minutes > 48 && minutes <= 59) {
+        res.status(408)
+        res.render('408')
+    } else {
+        console.log('ok')
+    }
+    next()
+})
+
 app.use('/static/css', express.static('public/css'))
 app.use('/static/js', express.static('public/js'))
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(router)
+
+/**
+ * Si la route n'est pas trouvé, nous renvoyons une erreur 404. (not found)
+ */
+router.get('*', (req, res, next) => {
+    res.status(404)
+    res.render('404')
+})
 
 app.listen(8000, (err) => {
     if(err) {
